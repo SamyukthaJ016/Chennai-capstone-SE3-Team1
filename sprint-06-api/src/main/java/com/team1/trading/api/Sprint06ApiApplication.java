f38c58677893ce59fbc466f6c1769c76a85ac54d
Sai Ethihas Chanda <127788928+SaiEthihasChanda@users.noreply.github.com>
2026-09-07 19:53:03 +0530

    spring contract

---
+++ sprint-06-api/src/main/java/com/team1/trading/api/Sprint06ApiApplication.java
@@ -0,0 +1,13 @@
+package com.team1.trading.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint06ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sprint06ApiApplication.class, args);
+    }
+
+}
---
+++ sprint-06-api/src/test/java/com/team1/trading/api/Sprint06ApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.team1.trading.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class Sprint06ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
